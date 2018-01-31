cd9a5f3fd4713b2ec0a9c2c7b95be6d390a4b269
Daktari <dindijjames@gmail.com>
2018-01-31 15:52:28 +0000

    Configure test database

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 # app/__init__.py
 
 # third party imports
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager 
 from flask_migrate import Migrate
@@ -41,6 +41,19 @@ def create_app(config_name):
     
     from app import models
     
+    @app.errorhandler(403)
+    def forbidden(error):
+        return render_template('errors/403.html', title="Forbidden"), 403
+        
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template('errors/404.html', title="Page Not Found"), 404
+        
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return render_template('errors/500.html', title="Internal Server Error"), 500
+    
     return app
+    
     
     
