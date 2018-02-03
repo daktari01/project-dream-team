03544d6b40397a8f71966a9e3aa435a49bc748ed
James Dindi <dindijjames@gmail.com>
2018-02-03 14:26:37 +0300

    Add tests for error pages

--- tests.py
+++ tests.py
@@ -115,7 +115,76 @@ class TestViews(TestBase):
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, redirect_url)
 
-    
+    def test_admin_dashboard_view(self):
+        """
+        Test that admin dashboard is inaccessible withput login
+        and redirects to login page then to dashboard
+        """
+        target_url = url_for('home.admin_dashboard')
+        redirect_url = url_for('auth.login', next=target_url)
+        response = self.client.get(target_url)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, redirect_url)
+
+    def test_department_view(self):
+        """
+        Test that departments page is inaccessible without login
+        and redirects to login page then to departments page
+        """
+        target_url = url_for('admin.list_departments')
+        redirect_url = url_for('auth.login', next=target_url)
+        response = self.client.get(target_url)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, redirect_url)
+
+    def test_roles_view(self):
+        """
+        Test that roles page is inaccessible without login
+        and redirects to login page then to roles page
+        """
+        target_url = url_for('admin.list_roles')
+        redirect_url = url_for('auth.login', next=target_url)
+        response = self.client.get(target_url)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, redirect_url)
+
+    def test_employees_view(self):
+        """
+        Test that employees page is inaccessible without login
+        and redirects to login page then to employees page
+        """
+        target_url = url_for('admin.list_employees')
+        redirect_url = url_for('auth.login', next=target_url)
+        response = self.client.get(target_url)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, redirect_url)
+
+class TestErrorPages(TestBase):
+
+    def test_403_forbidden(self):
+        # Create route to abort the request with the 403 error
+        @self.app.route('/403')
+        def forbidden_error():
+            abort(403)
+        response = self.client.get('/403')
+        self.assertEqual(response.status_code, 403)
+        self.assertTrue("403 Error" in response.data)
+
+    def test_404_not_found(self):
+        response = self.client.get('/nothinghere')
+        self.assertEqual(response.status_code, 404)
+        self.assertTrue("404 Error" in response.data)
+
+    def test_500_internal_server_error(self):
+        # Create route to abort the request with 500 the error
+        @self.app.route('/500')
+        def internal_server_error():
+            abort(500)
+
+        response = self.client.get('/500')
+        self.assertEqual(response.status_code, 500)
+        self.assertTrue("500 Error" in response.data)
+
 
 if __name__ == '__main__':
     unittest.main()
