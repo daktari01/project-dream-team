08429d5f08face9b1d9874b223b534309b91a4bf
James Dindi <dindijjames@gmail.com>
2018-02-11 21:27:45 +0300

    Setting up selenium

--- run.py
+++ run.py
@@ -9,4 +9,5 @@ app = create_app(config_name)
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', port=8080, debug=True) # c9
-    app.run()
+    app.run(debug=True)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
